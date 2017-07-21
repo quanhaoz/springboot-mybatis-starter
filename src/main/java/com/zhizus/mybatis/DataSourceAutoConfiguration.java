47f385a9103f4837a6f5c7239929abab8b1f9073
dempezheng <dempezheng@kugou.net>
2017-07-21 19:10:30 +0800

    init

--- src/main/java/com/zhizus/mybatis/DataSourceAutoConfiguration.java
+++ src/main/java/com/zhizus/mybatis/DataSourceAutoConfiguration.java
@@ -20,10 +20,8 @@ import org.springframework.context.annotation.Configuration;
 import tk.mybatis.spring.mapper.MapperScannerConfigurer;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by dempezheng on 2017/7/20.
@@ -33,91 +31,72 @@ import java.util.Set;
 public class DataSourceAutoConfiguration implements BeanDefinitionRegistryPostProcessor {
     private static final Logger LOGGER = LoggerFactory.getLogger(DataSourceAutoConfiguration.class);
 
+    ConcurrentHashMap<String, DataSource> groupDataSourceMap = Maps.newConcurrentMap();
+
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
     }
 
     @Override
-    public void postProcessBeanDefinitionRegistry(
-            BeanDefinitionRegistry registry) throws BeansException {
-
-
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
         if (initDefaultDataSource(registry)) {
             LOGGER.info("has init default datasource");
         }
 
-        if (!ExtraConf.hasExtraConf(ExtraConf.ConfType.MYSQL)) {
-            return;
-        }
-        Set<String> sourceNames = ExtraConf.getInitBeanNames(ExtraConf.ConfType.MYSQL);
-        if (sourceNames == null || sourceNames.isEmpty()) {
-            return;
-        }
-        for (String sourceName : sourceNames) {
-            try {
-                ExtraConf.Conf conf = ExtraConf.getConfig(ExtraConf.ConfType.MYSQL, sourceName);
-
-                String basePackage = conf.getProperty("basePackage");
-
-                LOGGER.info("registry extra datasource {} , basePackage :{}", new Object[]{sourceName, basePackage});
-
-                BeanDefinitionBuilder sessionFactoryDefinition = BeanDefinitionBuilder.genericBeanDefinition(DefaultSqlSessionFactory.class);
-                DataSource dataSource = DataSourceBuilder.createDataSource(conf);
-                SqlSessionFactory sessionFactory = DataSourceBuilder.createSqlSessionFactory(dataSource, conf.getProperty("mappers"));
-                sessionFactoryDefinition.addConstructorArgValue(sessionFactory.getConfiguration());
-
-                String sessionFactoryName = "sessionFactory@" + sourceName;
-                //定义sessionFactory
-                registry.registerBeanDefinition(sessionFactoryName, sessionFactoryDefinition.getBeanDefinition());
-
-
-                BeanDefinitionBuilder mapperScannerConfigurer = BeanDefinitionBuilder.genericBeanDefinition(MapperScannerConfigurer.class);
-                mapperScannerConfigurer.addPropertyValue("sqlSessionFactoryBeanName", sessionFactoryName);
-                mapperScannerConfigurer.addPropertyValue("annotationClass", MyBatisRepository.class);
-                mapperScannerConfigurer.addPropertyValue("basePackage", basePackage);
-
-                //定义扫描的路径与注解类
-                registry.registerBeanDefinition("mapperScanner@" + sourceName, mapperScannerConfigurer.getBeanDefinition());
-
-
-            } catch (Exception e) {
-                LOGGER.error("register extra datasource: {} error ", sourceName, e);
-            }
+        try {
+            loadMapperScanner(registry);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
         }
 
     }
 
-    Map<String, DataSource> groupDataSourceMap = Maps.newConcurrentMap();
+    private void registerScanner(BeanDefinitionRegistry registry, String sourceName, String mappers, String basePackage, DataSource dataSource) throws Exception {
+        BeanDefinitionBuilder sessionFactoryDefinition = BeanDefinitionBuilder.genericBeanDefinition(DefaultSqlSessionFactory.class);
 
-    private void loadMapperScanner(  BeanDefinitionRegistry registry) throws SQLException {
+        SqlSessionFactory sessionFactory = DataSourceBuilder.createSqlSessionFactory(dataSource, mappers);
+        sessionFactoryDefinition.addConstructorArgValue(sessionFactory.getConfiguration());
+
+        String sessionFactoryName = "sessionFactory@" + sourceName;
+        //定义sessionFactory
+        registry.registerBeanDefinition(sessionFactoryName, sessionFactoryDefinition.getBeanDefinition());
+
+
+        BeanDefinitionBuilder mapperScannerConfigurer = BeanDefinitionBuilder.genericBeanDefinition(MapperScannerConfigurer.class);
+        mapperScannerConfigurer.addPropertyValue("sqlSessionFactoryBeanName", sessionFactoryName);
+        mapperScannerConfigurer.addPropertyValue("annotationClass", MyBatisRepository.class);
+        mapperScannerConfigurer.addPropertyValue("basePackage", basePackage);
+
+        //定义扫描的路径与注解类
+        registry.registerBeanDefinition("mapperScanner@" + sourceName, mapperScannerConfigurer.getBeanDefinition());
+    }
+
+
+    private void loadMapperScanner(BeanDefinitionRegistry registry) throws Exception {
         Config load = ConfigFactory.load("application.conf");
         Config group = load.getConfig("mybatis").getConfig("group");
         String basePackage = group.getString("package");
+        String mappers = group.getString("mappers");
+        String name = group.getString("name");
         List<? extends Config> configList = group.getConfigList("source-group");
         //当datasource-group只有一个的时候，不需要自动切换
         if (configList.size() == 1) {
             Config config = configList.get(0);
             DataSource dataSource = DataSourceBuilder.createDataSource(config);
+            registerScanner(registry, name, mappers, basePackage, dataSource);
 
-
+        } else {
+            for (Config config : configList) {
+                String groupKey = config.getString("group");
+                DataSource dataSource = DataSourceBuilder.createDataSource(config);
+                groupDataSourceMap.put(groupKey, dataSource);
+            }
+            DataSource dynamicDataSource = new DynamicDataSource(groupDataSourceMap);
+            registerScanner(registry, name, mappers, basePackage, dynamicDataSource);
         }
-        for (Config config : configList) {
-            String groupKey = config.getString("group");
-            DataSource dataSource = DataSourceBuilder.createDataSource(config);
-            groupDataSourceMap.put(groupKey, dataSource);
 
-        }
     }
 
-    private void registerSqlSessionFactory( BeanDefinitionRegistry registry,DataSource dataSource,String mappers,String groupKey) throws Exception {
-        BeanDefinitionBuilder sessionFactoryDefinition = BeanDefinitionBuilder.genericBeanDefinition(DefaultSqlSessionFactory.class);
-        SqlSessionFactory sessionFactory = DataSourceBuilder.createSqlSessionFactory(dataSource,mappers );
-        sessionFactoryDefinition.addConstructorArgValue(sessionFactory.getConfiguration());
-
-        String sessionFactoryName = "sessionFactory@" + groupKey;
-        //定义sessionFactory
-        registry.registerBeanDefinition(sessionFactoryName, sessionFactoryDefinition.getBeanDefinition());
-    }
 
     public boolean initDefaultDataSource(BeanDefinitionRegistry registry) {
         BeanDefinition beanDefinition = registry.getBeanDefinition("dataSource");
--- src/main/java/com/zhizus/mybatis/DynamicDataSource.java
+++ src/main/java/com/zhizus/mybatis/DynamicDataSource.java
@@ -2,12 +2,22 @@ package com.zhizus.mybatis;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
+import javax.sql.DataSource;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * 1.监控mapper的执行状况，如果异常超过阈值，自动切换
  * 这里的lookUpKey可根据监控状况获取
  * Created by dempezheng on 2017/7/20.
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    private ConcurrentHashMap<String, DataSource> groupDataSourceMap;
+
+    public DynamicDataSource(ConcurrentHashMap<String, DataSource> groupDataSourceMap) {
+        this.groupDataSourceMap = groupDataSourceMap;
+    }
+
 
     @Override
     protected Object determineCurrentLookupKey() {
